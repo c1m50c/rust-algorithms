c5176a0f3f8703bd34635c02519e0c89db2e36a2
c1m50c <terminalityms@gmail.com>
2021-10-14 19:09:22 -0500

    linear search documentation

--- src/searching_algorithms/linear_search.rs
+++ src/searching_algorithms/linear_search.rs
@@ -2,6 +2,20 @@ use std::result::Result;
 use std::vec::Vec;
 
 
+/// # Linear Search
+/// Searches through `vec` to find `finding`, returning its index if found, or an error if not found.
+/// ### Parameters:
+/// ```rust
+/// vec: &Vec<i32> // Vector to search through.
+/// finding: i32 // What we're trying to find within the Vector.
+/// ```
+/// ### Complexities:
+/// ```py
+/// Worst Case Time Complexity == O(n)
+/// Average Case Time Complexity == O(n / 2)
+/// Best Case Time Complexity == O(1)
+/// Space Time Complexity == O(1)
+/// ```
 pub fn linear_search(vec: &Vec<i32>, finding: i32) -> Result<usize, &'static str> {
     for index in 0 .. vec.len() {
         if vec[index] == finding {
