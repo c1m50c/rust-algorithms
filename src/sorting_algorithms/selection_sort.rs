8fdfd25a04493206cd1b8cf9844884bf0f9edf6d
c1m50c <terminalityms@gmail.com>
2021-10-15 12:57:12 -0500

    selection_sort documentation

--- src/sorting_algorithms/selection_sort.rs
+++ src/sorting_algorithms/selection_sort.rs
@@ -1,6 +1,19 @@
 use std::vec::Vec;
 
 
+/// # Selection Sort
+/// ### Parameters:
+/// ```rust
+/// vec: &mut Vec<i32> // Vector to sort
+/// ```
+/// ### Complexities
+/// ```py
+/// Worst Case Time Complexity == O(n^2)
+/// Average Case Time Complexity == O(n^2)
+/// Best Case Time Complexity == O(n^2)
+/// Space Complexity == O(1)
+/// ```
+#[allow(dead_code)]
 pub fn selection_sort(vec: &mut Vec<i32>) {
     for i in 0 .. vec.len() - 1 {
         let mut cur_min_idx: usize = i;
