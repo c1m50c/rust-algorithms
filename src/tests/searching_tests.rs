e937dad38022b1d5cc764393882daaae4edd9bb6
c1m50c <terminalityms@gmail.com>
2021-10-14 19:48:02 -0500

    speed test

--- src/tests/searching_tests.rs
+++ src/tests/searching_tests.rs
@@ -1,6 +1,8 @@
 use super::helper_functions::create_integer_vector;
+use std::time::{Duration, Instant};
 use std::result::Result;
 use std::vec::Vec;
+use rand::Rng;
 
 
 /// # Run Tests
@@ -28,7 +30,15 @@ pub fn run_tests(func: &dyn Fn(&Vec<i32>, i32) -> Result<usize, &'static str>) {
 pub fn speed_test(func: &dyn Fn(&Vec<i32>, i32) -> Result<usize, &'static str>, length: i32) {
     println!("Search Speed Test");
 
-    let mut vec: Vec<i32> = create_integer_vector(length, RAND_MIN, RAND_MAX);
+    let vec: Vec<i32> = create_integer_vector(length, RAND_MIN, RAND_MAX);
     const RAND_MIN: i32 = -2500000;
     const RAND_MAX: i32 = 2500000;
+
+    println!("Vector Length: {}", vec.len());
+    let begin_time: Instant = Instant::now();
+    let finding: i32 = rand::thread_rng().gen_range(RAND_MIN .. RAND_MAX);
+    let found: Result<usize, &'static str> = func(&vec, finding);
+    let end_time: Duration = begin_time.elapsed();
+    println!("Searched for '{}', found at index '{}'.", finding, found.unwrap());
+    println!("Time Elapsed: {}ms", end_time.as_millis());
 }
