d35adb30113414b70b6c6d2a09e85961188fd41c
c1m50c <terminalityms@gmail.com>
2021-10-15 16:24:54 -0500

    bubble sort documentation

--- src/sorting_algorithms/bubble_sort.rs
+++ src/sorting_algorithms/bubble_sort.rs
@@ -1,6 +1,18 @@
 use std::vec::Vec;
 
 
+/// # Bubble Sort
+/// ### Parameters:
+/// ```rust
+/// vec: &mut Vec<i32> // Vector to sort.
+/// ```
+/// ### Complexities:
+/// ```py
+/// Worst Case Time Complexity == O(n^2)
+/// Average Case Time Complexity == O(n^2)
+/// Best Case Time Complexity == O(n)
+/// Space Complexity == O(n) total, O(1) auxiliary
+/// ```
 #[allow(dead_code)]
 pub fn bubble_sort(vec: &mut Vec<i32>) {
     for i in 0 .. vec.len() {
