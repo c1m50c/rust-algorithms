fa9e8b2380d9898b88bd7d5c86e8183cf3cb9bc4
c1m50c <terminalityms@gmail.com>
2021-10-21 18:31:12 -0500

    len optimized, documented merge_sort

--- src/sorting_algorithms/merge_sort.rs
+++ src/sorting_algorithms/merge_sort.rs
@@ -5,6 +5,11 @@ use std::vec::Vec;
 /// Deals with the merging and comparison aspect of `merge_sort`.
 /// ### Parameters:
 /// ```rust
+/// where T: Ord + Copy
+/// left: usize // Left Index.
+/// middle: usize // Middle Index.
+/// right: usize // Right Index.
+/// vec: &mut Vec<T> // Vector to merge into.
 /// ```
 #[allow(dead_code)]
 fn merge<T: Ord + Copy>(left: usize, middle: usize, right: usize, vec: &mut Vec<T>) {
@@ -15,11 +20,13 @@ fn merge<T: Ord + Copy>(left: usize, middle: usize, right: usize, vec: &mut Vec<
     for v in vec.iter().take(middle + 1).skip(left) { left_vec.push(*v); }
     for v in vec.iter().take(right + 1).skip(middle + 1) { right_vec.push(*v); }
 
+    let (left_len, right_len) = (left_vec.len(), right_vec.len());
+
     /* Track positions while merging */
     let (mut l, mut r, mut v) = (0, 0, left);
 
     /* Pick smaller elements one by one from both halves */
-    while l < left_vec.len() && r < right_vec.len() {
+    while l < left_len && r < right_len {
         if left_vec[l] < right_vec[r] {
             vec[v] = left_vec[l];
             l += 1;
@@ -33,14 +40,14 @@ fn merge<T: Ord + Copy>(left: usize, middle: usize, right: usize, vec: &mut Vec<
     }
 
     /* Finish up putting away all elements in left half */
-    while l < left_vec.len() {
+    while l < left_len {
         vec[v] = left_vec[l];
         l += 1;
         v += 1;
     }
 
     /* Finish up putting away all elements in right half */
-    while r < right_vec.len() {
+    while r < right_len {
         vec[v] = right_vec[r];
         r += 1;
         v += 1;
@@ -48,6 +55,22 @@ fn merge<T: Ord + Copy>(left: usize, middle: usize, right: usize, vec: &mut Vec<
 }
 
 
+/// # Merge Sort *LeftRight*
+/// Similar to `merge_sort` but with control over starting and end position of the sort.
+/// ### Parameters:
+/// ```rust
+/// where T: Ord + Copy
+/// vec: &mut Vec<T> // Vector to sort.
+/// left: usize // Starting index position.
+/// right: usize // Ending index position.
+/// ```
+/// ### Complexities:
+/// ```py
+/// Worst Case Time Complexity == O(n log n)
+/// Average Case Time Complexity == O(n log n)
+/// Best Case Time Complexity == O(n log n)
+/// Space Complexity == O(n)
+/// ```
 fn merge_sort_lr<T: Ord + Copy>(vec: &mut Vec<T>, left: usize, right: usize) {
     if left < right {
         let middle: usize = left + (right - left) / 2;
@@ -73,5 +96,6 @@ fn merge_sort_lr<T: Ord + Copy>(vec: &mut Vec<T>, left: usize, right: usize) {
 /// ```
 #[allow(dead_code)]
 pub fn merge_sort<T: Ord + Copy>(vec: &mut Vec<T>) {
-    if vec.len() > 1 { merge_sort_lr(vec, 0, vec.len() - 1); }
+    let vec_len: usize = vec.len();
+    if vec_len > 1 { merge_sort_lr(vec, 0, vec_len - 1); }
 }
