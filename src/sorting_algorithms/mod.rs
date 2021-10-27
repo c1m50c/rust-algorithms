792a2d0bb31a381991f5e1c045b26477a86bc199
c1m50c <terminalityms@gmail.com>
2021-10-26 21:32:23 -0500

    shell sort

--- src/sorting_algorithms/mod.rs
+++ src/sorting_algorithms/mod.rs
@@ -3,5 +3,6 @@ pub(crate) mod insertion_sort;
 pub(crate) mod bubble_sort;
 pub(crate) mod quick_sort;
 pub(crate) mod merge_sort;
+pub(crate) mod shell_sort;
 pub(crate) mod heap_sort;
 pub(crate) mod comb_sort;
