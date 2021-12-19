ff9d4866b91aed4c5e198fd48a9abf76d0207efb
c1m50c <terminalityms@gmail.com>
2021-12-19 01:14:35 -0600

    More Progress on the Computational Section of `SHA256` ✔

--- src/hashing/sha256.rs
+++ src/hashing/sha256.rs
@@ -12,11 +12,6 @@ use std::string::String;
 use std::vec::Vec;
 
 
-const H: [u32; 8] = [
-    0x6a09e667, 0xbb67ae85, 0x3c6ef372, 0xa54ff53a,
-    0x510e527f, 0x9b05688c, 0x1f83d9ab, 0x5be0cd19,
-];
-
 const K: [u32; 64] = [
     0x428a2f98, 0x71374491, 0xb5c0fbcf, 0xe9b5dba5, 0x3956c25b, 0x59f111f1, 0x923f82a4, 0xab1c5ed5,
     0xd807aa98, 0x12835b01, 0x243185be, 0x550c7dc3, 0x72be5d74, 0x80deb1fe, 0x9bdc06a7, 0xc19bf174,
@@ -66,35 +61,40 @@ pub fn sha256(message: String) -> String {
     assert_ne!(chunks.len(), 0, "Could not split message into any chunks.");
 
 
+    let mut hash: [u32; 8] = [
+        0x6a09e667, 0xbb67ae85, 0x3c6ef372, 0xa54ff53a,
+        0x510e527f, 0x9b05688c, 0x1f83d9ab, 0x5be0cd19,
+    ];
+
+
     /*
         Computation
         ===========
         |> TODO: Finish this section.
     */
     for c in chunks {
-        // TODO: Create Message Schedule
-        let mut message_schedule = Vec::<u32>::new();
-        for t in 0 .. 64 {
-            if t <= 15 {
-                // message_schedule.push(c[t * 4 : (t * 4) + 4])
-            } else {
+        let mut w = Vec::<u32>::with_capacity(64);
 
-            }
+        for t in 0 .. 64 {
+            /* FIXME: Conversion to u32 might result in miscalculation later? */
+            /* FIXME: Addition overflow error */
+            if t <= 15 { w.push(c[t] as u32); }
+            else { w.push(lc_sigma_1(w[t - 2]) + w[t - 7] + lc_sigma_0(w[t - 15]) + w[t - 16]); }
         }
 
-        assert_eq!(message_schedule.len(), 64, "Could not properly create a message schedule.");
+        assert_eq!(w.len(), 64, "Could not properly create a message schedule.");
 
-        let mut a = H[0];
-        let mut b = H[1];
-        let mut c = H[2];
-        let mut d = H[3];
-        let mut e = H[4];
-        let mut f = H[5];
-        let mut g = H[6];
-        let mut h = H[7];
+        let mut a = hash[0];
+        let mut b = hash[1];
+        let mut c = hash[2];
+        let mut d = hash[3];
+        let mut e = hash[4];
+        let mut f = hash[5];
+        let mut g = hash[6];
+        let mut h = hash[7];
 
         for t in 0 .. 64 {
-            let t1: u32 = h + sigma_1(e) + ch(e, f, g) + K[t] + message_schedule[t];
+            let t1: u32 = h + sigma_1(e) + ch(e, f, g) + K[t] + w[t];
             let t2: u32 = sigma_0(a) + maj(a, b, c);
 
             h = g;
@@ -106,6 +106,15 @@ pub fn sha256(message: String) -> String {
             b = a;
             a = t1 + t2;
         }
+
+        hash[0] = a + hash[0];
+        hash[1] = b + hash[1];
+        hash[2] = c + hash[2];
+        hash[3] = d + hash[3];
+        hash[4] = e + hash[4];
+        hash[5] = f + hash[5];
+        hash[6] = g + hash[6];
+        hash[7] = h + hash[7];
     }
 
 
@@ -143,21 +152,23 @@ const fn lc_sigma_0(x: u32) -> u32 { rotate_right(x, 7) ^ rotate_right(x, 18) ^ 
 const fn lc_sigma_1(x: u32) -> u32 { rotate_right(x, 17) ^ rotate_right(x, 19) ^ (x >> 10) }
 
 
-// #[cfg(test)]
-// mod tests {
-//     use super::sha256;
+#[cfg(test)]
+mod tests {
+    use super::sha256;
 
-//     #[test]
-//     fn hash_one() {
-//         let to_be_hashed: &str = "Hello, World!";
-//         let proper_hash: &str = "DFFD6021BB2BD5B0AF676290809EC3A53191DD81C7F70A4B28688A362182986F";
-//         assert_eq!(sha256(to_be_hashed.to_owned()), String::from(proper_hash));
-//     }
+    #[test]
+    #[ignore]
+    fn hash_one() {
+        let to_be_hashed: &str = "Hello, World!";
+        let proper_hash: &str = "DFFD6021BB2BD5B0AF676290809EC3A53191DD81C7F70A4B28688A362182986F";
+        assert_eq!(sha256(to_be_hashed.to_owned()), String::from(proper_hash));
+    }
 
-//     #[test]
-//     fn hash_two() {
-//         let to_be_hashed: &str = "qwertypassu321";
-//         let proper_hash: &str = "D8A5EC5F100B86C9CAD1AB984E0C2AF3D045AE6CFC9529A6F7C9CD0678E719D1";
-//         assert_eq!(sha256(to_be_hashed.to_owned()), String::from(proper_hash));
-//     }
-// }
+    #[test]
+    #[ignore]
+    fn hash_two() {
+        let to_be_hashed: &str = "qwertypassword1";
+        let proper_hash: &str = "D8A5EC5F100B86C9CAD1AB984E0C2AF3D045AE6CFC9529A6F7C9CD0678E719D1";
+        assert_eq!(sha256(to_be_hashed.to_owned()), String::from(proper_hash));
+    }
+}
